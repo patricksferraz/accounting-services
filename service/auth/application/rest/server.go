c8317d2ec5d0e83e80566e29b53d1a28a1bd1d48
patricksferraz <patrick.ferraz@outlook.com>
2021-05-28 23:49:06 -0300

    feat: adds swagger and CORS

--- service/auth/application/rest/server.go
+++ service/auth/application/rest/server.go
@@ -4,22 +4,38 @@ import (
 	"fmt"
 	"log"
 
+	_ "github.com/c4ut/accounting-services/service/auth/application/rest/docs"
 	_service "github.com/c4ut/accounting-services/service/auth/domain/service"
 	"github.com/c4ut/accounting-services/service/auth/infrastructure/external"
 	"github.com/c4ut/accounting-services/service/auth/infrastructure/repository"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"go.elastic.co/apm/module/apmgin"
 )
 
+// @title Auth Swagger API
+// @version 1.0
+// @description Swagger API for Golang Project Auth.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Coding4u
+// @contact.email comercial@coding4u.com.br
+
+// @BasePath /api/v1
 func StartRestServer(service *external.Keycloak, port int) {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(cors.Default())
 	r.Use(apmgin.Middleware(r))
 
 	authRepository := repository.NewAuthRepository(service)
 	authService := _service.NewAuthService(authRepository)
 	authRestService := NewAuthRestService(authService)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	v1 := r.Group("api/v1")
 	{
